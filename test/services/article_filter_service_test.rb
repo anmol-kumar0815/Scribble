a3d144d0a6db1b0f9b3379c60d98e4e5f9b3e467
Anmol Kumar <67572237+anmol-kumar0815@users.noreply.github.com>
2026-03-01 22:59:36 +0530

    Rewrite article filter service unit tests for Searchkick

--- test/services/article_filter_service_test.rb
+++ test/services/article_filter_service_test.rb
@@ -2,36 +2,67 @@
 
 require "test_helper"
 
-class ArticleFilterServiceTest < ActionDispatch::IntegrationTest
+class ArticleFilterServiceTest < ActiveSupport::TestCase
   def setup
-    @organization = create(:organization)
-    @user = create(:user, organization: @organization)
-    @category = create(:category, user: @user)
-    @article = create(:article, category: @category, user: @user)
+    @user = create(:user)
+    @selected_category_ids = [1, 2]
+    @searched_term = "  ruby on rails  "
   end
 
-  def test_should_filter_article_by_selected_status
-    first_test_article = create(:article, category: @category, user: @user)
-    second_test_article = create(:article, category: @category, user: @user)
-    filtered_articles = ArticleFilterService.new(@user.articles, [], "Draft", "").process
+  def test_process_builds_filters_with_all_options
+    expected_results = [build(:article)]
 
-    assert_equal @user.articles.where(status: "Draft").count, filtered_articles.count
+    Article.expects(:search).with(
+      "ruby on rails",
+      where: {
+        user_id: @user.id,
+        category_id: @selected_category_ids,
+        status: "Published"
+      },
+      order: { updated_at: :desc }
+    ).returns(expected_results)
+
+    result = ArticleFilterService.new(
+      @user.id,
+      @selected_category_ids,
+      "Published",
+      @searched_term
+    ).process
+
+    assert_equal expected_results, result
   end
 
-  def test_should_filter_article_by_searched_title
-    first_test_article = create(:article, title: "dummy", category: @category, user: @user)
-    second_test_article = create(:article, title: "test", category: @category, user: @user)
-    filtered_articles = ArticleFilterService.new(@user.articles, [], "All", "dum").process
+  def test_process_uses_wildcard_when_search_term_blank
+    expected_results = []
 
-    assert_equal [first_test_article], filtered_articles
+    Article.expects(:search).with(
+      "*",
+      where: { user_id: @user.id },
+      order: { updated_at: :desc }
+    ).returns(expected_results)
+
+    result = ArticleFilterService.new(@user.id, [], "All", "   ").process
+
+    assert_equal expected_results, result
   end
 
-  def test_should_filter_article_by_selected_category_ids
-    test_category = create(:category, user: @user)
-    first_test_article = create(:article, category: @category, user: @user)
-    second_test_article = create(:article, category: test_category, user: @user)
-    filtered_articles = ArticleFilterService.new(@user.articles, [test_category.id], "All", "").process
+  def test_process_does_not_include_status_filter_when_all_is_selected
+    Article.expects(:search).with(
+      "hello",
+      where: { user_id: @user.id, category_id: @selected_category_ids },
+      order: { updated_at: :desc }
+    ).returns([])
 
-    assert_equal [second_test_article], filtered_articles
+    ArticleFilterService.new(@user.id, @selected_category_ids, "All", "hello").process
+  end
+
+  def test_process_does_not_include_category_filter_when_no_categories_selected
+    Article.expects(:search).with(
+      "hello",
+      where: { user_id: @user.id, status: "Draft" },
+      order: { updated_at: :desc }
+    ).returns([])
+
+    ArticleFilterService.new(@user.id, nil, "Draft", "hello").process
   end
 end
